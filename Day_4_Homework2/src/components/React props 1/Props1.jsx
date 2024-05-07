1c59710913490560407bceacb8c27065bc0e6c4a
Aniket jauhri <aniketjauhri2003@gmail.com>
2024-05-07 15:35:05 +0530

    done

--- Day_4_Homework2/src/components/React props 1/Props1.jsx
+++ Day_4_Homework2/src/components/React props 1/Props1.jsx
@@ -2,23 +2,26 @@ import "./Props1.css";
 import data from "./data1";
 function Props1() {
   return (
-    <div className="props1">
-      {data.map((ele) => {
-        return (
-          <div className="props_card" key={ele.colorName}>
-            <div
-              style={{
-                backgroundColor: ele.colorCode,
-                width: "100px",
-                height: "100px",
-              }}
-            ></div>
-            <h2>{ele.colorCode}</h2>
-            <h3>{ele.colorName}</h3>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <h1>Props-1</h1>
+      <div className="props1">
+        {data.map((ele) => {
+          return (
+            <div className="props_card" key={ele.colorName}>
+              <div
+                style={{
+                  backgroundColor: ele.colorCode,
+                  width: "100px",
+                  height: "100px",
+                }}
+              ></div>
+              <h2>{ele.colorCode}</h2>
+              <h3>{ele.colorName}</h3>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 export default Props1;
--- Day_4_Homework2/src/components/React props 2/Props2.jsx
+++ Day_4_Homework2/src/components/React props 2/Props2.jsx
@@ -4,6 +4,7 @@ import Props2_card from "./Props2_card/Props2_card";
 function Props2() {
   return (
     <div>
+      <h1>Props-2</h1>
       {data.map((ele) => {
         return <Props2_card key={ele.genre} value={ele} />;
       })}
