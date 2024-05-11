ecf7a51f6758662356c51219d8ec8d7240fd1a85
Aniket jauhri <aniketjauhri2003@gmail.com>
2024-05-11 23:06:52 +0530

    done

---
+++ Day_7_Educator_Task/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Restaurant from "./Components/Restaurant/Restaurant";
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Restaurant />
+    </>
+  );
+}
+
+export default App;
